a9ab61873839b0721f749fab7da7094eda1c8e8c
Vitor Valcarcel <vitorvalcarcel@gmail.com>
2025-12-03 22:08:36 -0300

    first commit

---
+++ src/main/java/com/concurso/aprovaflow/AprovaFlowApplication.java
@@ -0,0 +1,13 @@
+package com.concurso.aprovaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprovaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprovaFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurso/aprovaflow/AprovaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurso.aprovaflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprovaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
